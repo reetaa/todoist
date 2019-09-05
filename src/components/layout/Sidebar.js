02713c0d716b3346e72129e84b8f620c1ba8df04
Reeta Shrestha <retdream@gmail.com>
2019-09-06 09:42:58 +1000

    Fix a11y and some style issues

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -6,13 +6,13 @@ import {
   FaRegCalendar
 } from "react-icons/fa";
 import { Projects } from "../Projects";
-import { AddProject } from "../AddProject";
 import { useSelectedProjectValue } from "../../context";
+import { AddProject } from "../AddProject";
 
 export const Sidebar = () => {
-  const [showProjects, setShowProjects] = useState(true);
   const { setSelectedProject } = useSelectedProjectValue();
   const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
 
   return (
     <div className="sidebar" data-testid="sidebar">
@@ -20,55 +20,94 @@ export const Sidebar = () => {
         <li
           data-testid="inbox"
           className={active === "inbox" ? "active" : undefined}
-          onClick={() => {
-            setActive("inbox");
-            setSelectedProject("INBOX");
-          }}
         >
-          <span>
-            <FaInbox />
-          </span>
-          <span>Inbox</span>
+          <div
+            data-testid="inbox-action"
+            aria-label="Show inbox tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive("inbox");
+              setSelectedProject("INBOX");
+            }}
+            onKeyDown={() => {
+              setActive("inbox");
+              setSelectedProject("INBOX");
+            }}
+          >
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
         </li>
         <li
           data-testid="today"
           className={active === "today" ? "active" : undefined}
-          onClick={() => {
-            setActive("today");
-            setSelectedProject("TODAY");
-          }}
         >
-          <span>
-            <FaRegCalendar />
-          </span>
-          <span>Today</span>
+          <div
+            data-testid="today-action"
+            aria-label="Show today's tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive("today");
+              setSelectedProject("TODAY");
+            }}
+            onKeyDown={() => {
+              setActive("today");
+              setSelectedProject("TODAY");
+            }}
+          >
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
         </li>
         <li
           data-testid="next_7"
           className={active === "next_7" ? "active" : undefined}
-          onClick={() => {
-            setActive("next_7");
-            setSelectedProject("NEXT_7");
-          }}
         >
-          <span>
-            <FaRegCalendarAlt />
-          </span>
-          <span>Next 7 days</span>
+          <div
+            data-testid="next_7-action"
+            aria-label="Show tasks for the next 7 days"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive("next_7");
+              setSelectedProject("NEXT_7");
+            }}
+            onKeyDown={() => {
+              setActive("next_7");
+              setSelectedProject("NEXT_7");
+            }}
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
         </li>
-        <div
-          className="sidebar__midle"
-          onClick={() => setShowProjects(!showProjects)}
-        >
-          <span>
-            <FaChevronDown
-              className={!showProjects ? "hidden-projects" : undefined}
-            />
-            <h2>Projects</h2>
-          </span>
-        </div>
       </ul>
+      <div
+        className="sidebar__middle"
+        aria-label="Show/hide projects"
+        onClick={() => setShowProjects(!showProjects)}
+        onKeyDown={() => setShowProjects(!showProjects)}
+        role="button"
+        tabIndex={0}
+      >
+        <span>
+          <FaChevronDown
+            className={!showProjects ? "hidden-projects" : undefined}
+          />
+        </span>
+        <h2>Projects</h2>
+      </div>
+
       <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+
       {showProjects && <AddProject />}
     </div>
   );
