09245387b541e25bd68ceaa3796987cad9d80ff3
Reeta Shrestha <retdream@gmail.com>
2019-09-06 12:53:46 +1000

    Updates

---
+++ precache-manifest.c90b6ede5d2b3fdee923d81a0070c624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c599925bf027906af027387b5431d7",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "577082ef081e290b4847",
+    "url": "/todoist/static/css/main.5d6179a4.chunk.css"
+  },
+  {
+    "revision": "fd4411fa3e74fc58b549",
+    "url": "/todoist/static/js/2.733dfb68.chunk.js"
+  },
+  {
+    "revision": "577082ef081e290b4847",
+    "url": "/todoist/static/js/main.fece3f34.chunk.js"
+  },
+  {
+    "revision": "d4138f1311a585634d1f",
+    "url": "/todoist/static/js/runtime~main.caa4752d.js"
+  }
+]);
